78dba5250b9c8169762788c960fa50f8628aba42
hafiz703 <mhbt-703@hotmail.com>
2017-04-10 14:01:12 +0800

    Second Commit

--- notForProject/osFile.py
+++
@@ -1,5 +0,0 @@
-import os
-x = os.listdir("C:\\Users\\-\\Desktop\\opencv-lane-vehicle-track-master\\New folder\\opencv\\build\\x86\\vc10\\lib")
-
-for i in x:
-	print i
--- notForProject/pipipy.py
+++
@@ -1,7 +0,0 @@
-import pip
-
-
-
-pip.main(['install','urllib3'])
-
-#pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/windows/cpu/tensorflow-1.0.1-cp35-cp35m-win_amd64.whl 
